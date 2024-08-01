dfb232803977ea57e36b43412ebff0c4a4f1e5fc
chelangatbrigit <brigitchelangat@gmail.com>
2024-08-01 15:37:05 +0300

    Added Member Model

--- Members/models.py
+++ Members/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, unique=True)
+    join_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
