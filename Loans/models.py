a25c12fbcf403a15f588e779d0089a5a19fd5e5e
chelangatbrigit <brigitchelangat@gmail.com>
2024-08-01 16:37:18 +0300

    All initial models done

--- Loans/models.py
+++ Loans/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Loan(models.Model):
+    LOAN_STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('approved', 'Approved'),
+        ('repaid', 'Repaid'),
+    ]
+
+    member_id = models.IntegerField()
+    loan_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    loan_date = models.DateField()
+    due_date = models.DateField()
+    status = models.CharField(max_length=10, choices=LOAN_STATUS_CHOICES)
+
+    def __str__(self):
+        return f"Loan of {self.loan_amount} to {self.member} on {self.loan_date}"
--- Savings/models.py
+++ Savings/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Saving(models.Model):
+    TRANSACTION_TYPE_CHOICES = [
+        ('deposit', 'Deposit'),
+        ('withdrawal', 'Withdrawal'),
+    ]
+
+    member_id = models.IntegerField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPE_CHOICES)
+    transaction_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.transaction_type.capitalize()} of {self.amount} on {self.transaction_date}"
