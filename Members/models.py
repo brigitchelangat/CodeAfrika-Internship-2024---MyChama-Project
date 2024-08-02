d1bd922cb80bff47b0016be36a5dcf613437efa5
chelangatbrigit <brigitchelangat@gmail.com>
2024-08-02 17:08:20 +0300

    Final Member Model

--- Members/models.py
+++ Members/models.py
@@ -1,11 +1,24 @@
 from django.db import models
 
+
+ACTIVE_STATUS_CHOICES = [
+        (0, 'Yes'),
+        (1, 'No'),    
+    ]
+
+IS_STAFF_CHOICES = [
+        (0, 'Yes'),
+        (1, 'No'),    
+    ]
+
 class Member(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    id_no = models.IntegerField(default=0)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, unique=True)
-    join_date = models.DateField()
+    profession = models.CharField(max_length=50,default="Unspecified")
+    joining_date = models.DateField()
+    is_active = models.CharField(max_length=5, choices=ACTIVE_STATUS_CHOICES, default=0)
+    is_staff = models.CharField(max_length=5, choices=IS_STAFF_CHOICES, default=1)
 
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
