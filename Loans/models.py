b5e22ef30c1ae5b583f36d7ec0590f51c21dcfec
chelangatbrigit <brigitchelangat@gmail.com>
2024-08-02 17:20:48 +0300

    Final Loan Model

--- Loans/models.py
+++ Loans/models.py
@@ -1,19 +1,26 @@
 from django.db import models
 
-# Create your models here.
 
 class Loan(models.Model):
     LOAN_STATUS_CHOICES = [
-        ('pending', 'Pending'),
-        ('approved', 'Approved'),
-        ('repaid', 'Repaid'),
+        (0, 'Pending'),
+        (1, 'Approved'),
+        (2, 'Rejected'),
+    ]
+    REPAID_CHOICES = [
+        (0, 'Yes'),
+        (1, 'No'),
     ]
 
     member_id = models.IntegerField()
+    ref_no = models.CharField(max_length=20,default="Unspecified")
     loan_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    amount_due = models.DecimalField(max_digits=10, decimal_places=2, default= 0.00)
+    interest = models.DecimalField(max_digits=10, decimal_places=2, default= 0.00)
+    repaid_amount = models.DecimalField(max_digits=10, decimal_places=2, default= 0.00)
+    loan_balance = models.DecimalField(max_digits=10, decimal_places=2, default= 0.00)
     loan_date = models.DateField()
     due_date = models.DateField()
     status = models.CharField(max_length=10, choices=LOAN_STATUS_CHOICES)
-
-    def __str__(self):
-        return f"Loan of {self.loan_amount} to {self.member} on {self.loan_date}"
+    comments = models.CharField(max_length=255,default="Unspecified")
+    repaid = models.CharField(max_length=5, choices=REPAID_CHOICES, default=1)
